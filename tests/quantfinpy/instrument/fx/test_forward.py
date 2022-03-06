9dfe040f679c20e86e6ea06655969e952254b177
TradingPy <tradingpython2022@gmail.com>
2022-03-06 00:23:00 +0000

    Adding comments to existing instrument testing

--- tests/quantfinpy/instrument/fx/test_forward.py
+++ tests/quantfinpy/instrument/fx/test_forward.py
@@ -8,10 +8,13 @@ from quantfinpy.enum.currency import Currency
 
 
 def test_forward_ctor():
+    # Creating a fx forward as a forward on fx spot.
     fx_spot = FXSpot(Currency.USD, Currency.EUR)
     strike: float = 1.0
     maturity: date = date.today()
     fx_fwd = FXForward(fx_spot, 1.0, maturity)
+
+    # Checking built fx spot.
     assert isinstance(fx_fwd, FXForward)
     assert fx_fwd.underlying == fx_spot
     assert fx_fwd.underlying_fx_spot == fx_spot
--- tests/quantfinpy/instrument/fx/test_spot.py
+++ tests/quantfinpy/instrument/fx/test_spot.py
@@ -5,10 +5,12 @@ from quantfinpy.enum.currency import Currency
 
 
 def test_fx_spot_ctor():
+    # Creating a fx spot.
     domestic_ccy: Currency = Currency.USD
     foreign_ccy: Currency = Currency.EUR
     fx_spot = FXSpot(domestic_ccy, foreign_ccy)
 
+    # Checking built fx spot.
     assert isinstance(fx_spot, FXSpot)
     assert fx_spot.domestic_currency == domestic_ccy
     assert fx_spot.foreign_currency == foreign_ccy
